e3771633b3d9e37d53abc81b0fdd0605b380b388
qkrwogns9703 <jaehunb350@gmail.com>
2020-11-26 15:07:30 +0900

    add login logout

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/login/login';
 import styles from './app.module.css';
+import Maker from './components/maker/maker';
 function App({ authService }) {
 	return (
 		<div className={styles.app}>
-			<Login authService={authService} />
+			<BrowserRouter>
+				<Switch>
+					<Route exact path='/'>
+						<Login authService={authService} />
+					</Route>
+					<Route path='/maker'>
+						<Maker authService={authService} />
+					</Route>
+				</Switch>
+			</BrowserRouter>
 		</div>
 	);
 }
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -5,6 +5,16 @@ class AuthService {
 		const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
 		return firebase.auth().signInWithPopup(authProvider);
 	}
+
+	logout() {
+		firebase.auth().signOut();
+	}
+
+	onAuthChange(onUserChanged) {
+		firebase.auth().onAuthStateChanged(user => {
+			onUserChanged(user);
+		});
+	}
 }
 
 export default AuthService;
