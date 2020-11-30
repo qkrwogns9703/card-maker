892d1dbd3192f5de8f3551de4c3c403b5e45cf91
qkrwogns9703 <jaehunb350@gmail.com>
2020-11-30 10:35:47 +0900

    complete

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -24,18 +24,17 @@ const Maker = ({ authService, FileInput, cardRepository }) => {
 		return () => {
 			stopSync();
 		};
-	}, [userId]);
+	}, [userId, cardRepository]);
 
 	useEffect(() => {
 		authService.onAuthChange(user => {
 			if (user) {
 				setUserId(user.uid);
-				console.log(userId);
 			} else {
 				history.push('/');
 			}
 		});
-	});
+	}, [userId, authService, history]);
 
 	const createOrUpdateCard = card => {
 		const updated = { ...cards };
--- src/service/card_repository.js
+++ src/service/card_repository.js
@@ -1,8 +1,8 @@
-import firebaseApp from './firebase';
+import { firebaseDatabase } from './firebase';
 
 class CardRepository {
 	syncCards(userId, onUpdate) {
-		const ref = firebaseApp.database().ref(`${userId}/cards`);
+		const ref = firebaseDatabase.ref(`${userId}/cards`);
 		ref.on('value', snapshot => {
 			const value = snapshot.val();
 			if (value) {
@@ -13,11 +13,11 @@ class CardRepository {
 	}
 
 	saveCard(userId, card) {
-		firebaseApp.database().ref(`${userId}/cards/${card.id}`).set(card);
+		firebaseDatabase.ref(`${userId}/cards/${card.id}`).set(card);
 	}
 
 	removeCard(userId, card) {
-		firebaseApp.database().ref(`${userId}/cards/${card.id}`).remove();
+		firebaseDatabase.ref(`${userId}/cards/${card.id}`).remove();
 	}
 }
 
